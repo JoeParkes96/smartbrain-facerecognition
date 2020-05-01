5e8656332e1c044d59d7652b33136bb610847715
JoeParkes96 <joeparkes@Joes-MacBook-Pro.local>
2020-05-01 16:45:39 +0100

    Updates

---
+++ precache-manifest.6471c6f450cd649cc33c692c5e217a2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06135129255ffd5f301bc81c7aebc051",
+    "url": "/smartbrain-facerecognition/index.html"
+  },
+  {
+    "revision": "bb34729c9466fb10057f",
+    "url": "/smartbrain-facerecognition/static/css/main.7a5a8e47.chunk.css"
+  },
+  {
+    "revision": "071a1a21df46e355f604",
+    "url": "/smartbrain-facerecognition/static/js/2.445a0651.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smartbrain-facerecognition/static/js/2.445a0651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb34729c9466fb10057f",
+    "url": "/smartbrain-facerecognition/static/js/main.dc62a9b8.chunk.js"
+  },
+  {
+    "revision": "5179b6419d8e58e5585e",
+    "url": "/smartbrain-facerecognition/static/js/runtime-main.7adae263.js"
+  },
+  {
+    "revision": "0e13690a1940a31adba9f1238a5435a6",
+    "url": "/smartbrain-facerecognition/static/media/logo_transparent.0e13690a.png"
+  }
+]);
